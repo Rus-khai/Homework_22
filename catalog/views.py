60a66fb7e7a4cdab0b197eefcac730dd01d9c387
ruslan <ruselkotya@gmail.com>
2025-12-04 23:57:06 +0300

    Add bootstrap CSS and contact page template

---
+++ catalog/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'catalog/home.html')
+
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        massage = request.POST.get('massage')
+
+        return HttpResponse(f'Спасибо, {name}! Сообщение получено.')
+    return render(request, 'catalog/contacts.html')
