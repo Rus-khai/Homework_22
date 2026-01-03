19b1c7b735cb86b58367a985370cf7987d61fc33
ruslan <ruselkotya@gmail.com>
2026-01-03 21:07:25 +0300

    finish homework

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,5 +5,7 @@ app_name = "catalog"
 
 urlpatterns = [
     path("home/", views.home, name="home"),
-    path("contacts/", views.contact, name="contacts"),
-]
+    path("catalog_list/", views.catalog_list, name="catalog_list"),
+    path("contacts/", views.contacts, name="contacts"),
+    path("products/<int:product_id>/", views.view_product, name="view_product"),
+    ]
